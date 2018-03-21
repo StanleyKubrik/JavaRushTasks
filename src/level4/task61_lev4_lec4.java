ef29a6a5f19bab122448c2d78861905fa167ba5c
StanleyKubrik <sukhonosov.nikita@gmail.com>
2018-03-21 17:34:13 +0200

    From work

---
+++ src/level4/task61_lev4_lec4.java
@@ -0,0 +1,20 @@
+package level4;
+
+import java.util.Scanner;
+
+public class task61_lev4_lec4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int year = in.nextInt();
+        int x;
+
+        if (year % 4 == 0 && year % 400 == 0){
+            x = 366;
+        } else {
+            x = 365;
+        }
+
+        System.out.println("Количество дней в году: " + x);
+    }
+}
