1aef227735a44a97ed8450237b66a96066feae6c
StanleyKubrik <sukhonosov.nikita@gmail.com>
2018-03-22 15:13:50 +0200

    From work

---
+++ src/level4/task72_lev4_lec7.java
@@ -0,0 +1,18 @@
+package level4;
+
+import java.io.*;
+
+public class task72_lev4_lec7 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String sa = reader.readLine();
+        int a = Integer.parseInt(sa);
+
+        if (a < 0 && a % 2 == 0) System.out.println("Отрицительное чётное число");
+        else if (a < 0 && a % 2 != 0) System.out.println("Отрицительное нечётное число");
+        else if (a == 0) System.out.println("Ноль");
+        else if (a > 0 && a % 2 == 0) System.out.println("Положительное чётное число");
+        else if (a > 0 && a % 2 != 0) System.out.println("Положительное нечётное число");
+    }
+}
---
+++ src/level4/task77_lev4_lec10.java
@@ -0,0 +1,12 @@
+package level4;
+
+public class task77_lev4_lec10 {
+    public static void main(String[] args) throws Exception{
+        int i = 10;
+
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
