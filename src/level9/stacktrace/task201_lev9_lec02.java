163303b81d59764ca44730142e5fd6fa6336c4d9
StanleyKubrik <sukhonosov.nikita@gmail.com>
2018-03-30 17:44:50 +0300

    From work 30/03/2018

---
+++ src/level9/stacktrace/task201_lev9_lec02.java
@@ -0,0 +1,31 @@
+package level9.stacktrace;
+
+public class task201_lev9_lec02 {
+    public static void main(String[] args) throws Exception {
+        method1();
+    }
+
+    public static int method1() {
+        method2();
+        return  /*напишите тут ваш код*/ 0;
+    }
+
+    public static int method2() {
+        method3();
+        return  /*напишите тут ваш код*/ 0;
+    }
+
+    public static int method3() {
+        method4();
+        return  /*напишите тут ваш код*/ 0;
+    }
+
+    public static int method4() {
+        method5();
+        return  /*напишите тут ваш код*/ 0;
+    }
+
+    public static int method5() {
+        return  /*напишите тут ваш код*/ 0;
+    }
+}
