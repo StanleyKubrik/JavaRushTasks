e46baa781b4e9b9019792fe48dc2ddb38696b0d1
StanleyKubrik <sukhonosov.nikita@gmail.com>
2018-04-03 22:31:53 +0300

    From home 03/04/2018

---
+++ src/javaCore/level12/task268_lev12_lec04.java
@@ -0,0 +1,47 @@
+/*
+Три метода и минимум
+Написать public static методы: int min(int, int), long min(long, long), double min(double, double).
+Каждый метод должен возвращать минимальное из двух переданных в него чисел.
+
+
+Требования:
+1. Программа не должна выводить текст на экран.
+2. Класс Solution должен содержать четыре метода.
+3. Класс Solution должен содержать статический метод int min(int, int).
+4. Метод int min(int, int) должен возвращать минимальное из двух чисел типа int.
+5. Класс Solution должен содержать статический метод long min(long, long).
+6. Метод long min(long, long) должен возвращать минимальное из двух чисел типа long.
+7. Класс Solution должен содержать статический метод double min(double, double).
+8. Метод double min(double, double) должен возвращать минимальное из двух чисел типа double.
+ */
+package javaCore.level12;
+
+public class task268_lev12_lec04 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int min(int a, int b){
+        if (a > b){
+            return b;
+        } else {
+            return a;
+        }
+    }
+
+    public static long min(long a, long b){
+        if (a > b){
+            return b;
+        } else {
+            return a;
+        }
+    }
+
+    public static double min(double a, double b){
+        if (a > b){
+            return b;
+        } else {
+            return a;
+        }
+    }
+}
