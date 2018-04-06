847605a4bf49c9aaae63f4317f427cec431085c4
StanleyKubrik <sukhonosov.nikita@gmail.com>
2018-04-06 09:06:49 +0300

    From work 06/04/2018

--- src/javaCore/level13/task310_lev13_lec11.java
+++ src/javaCore/level13/task310_lev13_lec11.java
@@ -1,19 +1,42 @@
+/*
+Писатель в файл с консоли
+1. Прочесть с консоли имя файла.
+2. Считывать строки с консоли, пока пользователь не введет строку "exit".
+3. Вывести абсолютно все введенные строки в файл, каждую строчку с новой строки.
+
+
+Требования:
+1. Программа должна считывать c консоли имя файла.
+2. Создай и используй объект типа BufferedWriter.
+3. Программа не должна ничего читать из файловой системы.
+4. Программа должна читывать строки с консоли, пока пользователь не введет строку "exit".
+5. Программа должна записать абсолютно все введенные строки (включая "exit") в файл, каждую строчку с новой строки.
+6. Метод main должен закрывать объект типа BufferedWriter после использования.
+ */
 package javaCore.level13;
 
 import java.io.*;
+import java.util.*;
 
 public class task310_lev13_lec11 {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        ArrayList<String> arrayList = new ArrayList<>();
 
-        InputStream in = new FileInputStream("c:/users/user/ideaprojects/javarushtasks/" + reader.readLine());
-
-        String str = reader.readLine();
-        while (!str.isEmpty()){
-
+        while (true){
+            String s = reader.readLine();
+            arrayList.add(s);
+            if (s.equals("exit")){ break; }
         }
 
-        in.close();
         reader.close();
+        File f = new File(name);
+        BufferedWriter writer = new BufferedWriter(new PrintWriter(f));
+        for (int i = 0; i < arrayList.size(); i++){
+            writer.write(arrayList.get(i));
+            writer.newLine();
+        }
+        writer.close();
     }
 }
