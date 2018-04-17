7526808fef8b72bae55039d8c9ffe83142eaacdd
StanleyKubrik <sukhonosov.nikita@gmail.com>
2018-04-17 20:56:12 +0300

    From home 17/04/2018

---
+++ src/javaCore/level17/task421_lev17_lec10/CorruptedDataException.java
@@ -0,0 +1,6 @@
+package javaCore.level17.task421_lev17_lec10;
+
+import java.io.IOException;
+
+public class CorruptedDataException extends IOException {
+}
---
+++ src/javaCore/level17/task421_lev17_lec10/Solution.java
@@ -0,0 +1,15 @@
+package javaCore.level17.task421_lev17_lec10;
+
+import java.util.*;
+
+public class Solution {
+    public static List<String> allLines = new ArrayList<String>();
+    public static List<String> forRemoveLines = new ArrayList<String>();
+
+    public static void main(String[] args) {
+    }
+
+    public void joinData () throws CorruptedDataException {
+
+    }
+}
